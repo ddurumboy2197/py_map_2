9fbf420c281ff7f90e5310e61515d0a82d599d11
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-17 17:01:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+nums = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, nums))
+print(doubled)  # [2, 4, 6, 8, 10]
+
+words = ["python", "map", "function", "lambda", "exercise"]
+lengths = list(map(len, words))
+print(lengths)  # [6, 3, 8, 6, 8]
